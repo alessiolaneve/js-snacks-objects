00f7e44c7e3f0cc995f967a335c92b07d344daf4
alessiolaneve <alessioln98@gmail.com>
2024-03-22 18:09:21 +0100

    inizio js4

--- js/app.js
+++ js/app.js
@@ -27,4 +27,32 @@ for (let i = 0; i < animali.length; i++) {
     if (animalsClass === "mammiferi") {
         mammiferi.push(animals)
     }
-} 
+}
+
+
+// JSNACK 4
+// Crea un array di oggetti che rappresentano delle persone.
+// Ogni persona ha un nome, un cognome e un’età.
+// Crea quindi un nuovo array inserendo, per ogni persona, una frase con il
+// nome e cognome e l’indicazione se può guidare, in base all’età.
+
+const persone = [
+    { nome: 'Paolo', cognome: 'Rossi', eta: 32 },
+    { nome: 'Marco', cognome: 'Gialli', eta: 20 },
+    { nome: 'Anita', cognome: 'Verdi', eta: 14 },
+    { nome: 'Max', cognome: 'Bianchi', eta: 15 },
+    { nome: 'Wendy', cognome: 'White', eta: 18 },
+]
+console.log(persone)
+const adulti = []
+
+for (let i = 0; i < persone.length; i++) {
+    const people = persone[i];
+    console.log(people)
+    const adults = people.eta
+    console.log(adults)
+
+    if (adults >= 18) {
+
+    }
+}
